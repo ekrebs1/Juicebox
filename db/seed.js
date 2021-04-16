63f2cb55d30faa990f4baa0f63b9b4124f528dca
Erica <ericakrebs1@gmail.com>
2021-04-15 20:43:00 -0500

    commit

--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,8 @@
 const {
     client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser
   } = require('./index');
   
 // this function should call a query which drops all tables from our database
@@ -29,8 +30,12 @@ async function dropTables() {
       await client.query(`
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
-        username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
+
       );
       `);
 
@@ -45,9 +50,9 @@ async function dropTables() {
     try {
       console.log("Starting to create users...");
   
-      const albert = await createUser({ username: 'albert', password: 'bertie99' });
-      const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-      const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
+      const albert = await createUser({ username: 'albert', password: 'bertie99', name:'Al Bert', location:'Sidney, Australia' });
+      const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name:'Just Sandra', location:"Ain't tellin" });
+      const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name:'Joshua', location:'Upper East Side' });
       console.log(albert, sandra, glamgal);
 
       
@@ -75,8 +80,16 @@ async function dropTables() {
     try {
       console.log("Starting to test database...");
   
+      console.log("Calling getAllUsers")
       const users = await getAllUsers();
-      console.log("getAllUsers:", users);
+      console.log("Result:", users);
+  
+      console.log("Calling updateUser on users[0]")
+      const updateUserResult = await updateUser(users[0].id, {
+        name: "Newname Sogood",
+        location: "Lesterville, KY"
+      });
+      console.log("Result:", updateUserResult);
   
       console.log("Finished database tests!");
     } catch (error) {
